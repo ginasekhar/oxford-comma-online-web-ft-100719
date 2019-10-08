1e29d1e380876cad00aedac7c397fd38c8fb4b42
Gina Sekhar <ginasekhar@yahoo.com>
2019-10-08 13:32:23 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -3,11 +3,11 @@ def oxford_comma(array)
  num_elements = array.size 
  if num_elements == 1 
    new_str = array[0]
-   return
+   return new_str
  end
  
  last_element = array[-1]
- 
-   
- end
+ new_str = (array.pop).join(", ")
+ new_str << "and #{last_element}"
+ return new_str
 end
