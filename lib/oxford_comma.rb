b6e34661ba5d56ee26791df75259ac2ccd02d785
Gina Sekhar <ginasekhar@yahoo.com>
2019-10-08 13:28:32 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,13 @@
 def oxford_comma(array)
-
+ new_str = ""
+ num_elements = array.size 
+ if num_elements == 1 
+   new_str = array[0]
+   return
+ end
+ 
+ last_element = array[-1]
+ 
+   
+ end
 end
