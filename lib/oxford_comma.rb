4c5d1b3029a74d81b0d2af0ecca4a07ee98cc34a
Gina Sekhar <ginasekhar@yahoo.com>
2019-10-08 13:35:13 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,13 +1,19 @@
+
+require 'pry'
 def oxford_comma(array)
  new_str = ""
  num_elements = array.size 
  if num_elements == 1 
    new_str = array[0]
+   binding.pry
    return new_str
  end
  
  last_element = array[-1]
+ last_element
  new_str = (array.pop).join(", ")
- new_str << "and #{last_element}"
+ binding.pry
+ new_str = new_str + "and #{last_element}"
+ binding.pry
  return new_str
 end
