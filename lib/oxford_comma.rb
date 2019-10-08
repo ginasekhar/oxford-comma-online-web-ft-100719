b018885a5ec42f7ad32f925f040cb6a496827de1
Gina Sekhar <ginasekhar@yahoo.com>
2019-10-08 13:46:41 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,20 @@
+
+require 'pry'
 def oxford_comma(array)
+ new_str = ""
+ num_elements = array.size 
+ case num_elements 
+  
+  when 1 
+   new_str = array[0]
+  when 2 
+    new_str = "#{array[0]} and #{array[1]}"
+  else
+    last_element = array.pop
+    new_str = array.join(", ")
+    new_str << ", and #{last_element}"
+  end
+ 
+  return new_str
 
 end
